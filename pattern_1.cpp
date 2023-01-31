74c60e62c22816c6660454cc11617d3ab6a68c94
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-01-31 17:16:09 +0530

    Add files via upload

---
+++ pattern_1.cpp
@@ -0,0 +1,29 @@
+/* 
+     1 2 3 4
+     1 2 3 4
+     1 2 3 4
+     1 2 3 4
+           */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<" Enter the value of n : ";
+    cin>>n;
+    int row =1;
+    while (row<=n)
+    {
+        int col=1;
+        while (col<=n)
+        {
+            cout <<col<<" ";
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    return 0;
+}
