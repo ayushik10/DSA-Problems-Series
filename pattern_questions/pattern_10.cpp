e7cca6e85959c708947fcfdee615ce063e8bdbbd
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-02-01 14:22:01 +0530

    Add files via upload

---
+++ pattern_questions/pattern_10.cpp
@@ -0,0 +1,30 @@
+/*
+    A B C D
+    A B C D
+    A B C D
+    A B C D
+                */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    int row = 1;
+
+    while (row <= n)
+    {
+        char ch = 'A';
+        int col = 1;
+        while (col <= n)
+        {
+            cout << ch << " ";
+            col++;
+            ch++;
+        }
+        cout << endl;
+        row++;
+    }
+    return 0;
+}
