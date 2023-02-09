a3238c1e09447dba608d44706a8a9a42b78362be
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-02-09 18:42:10 +0530

    Update counting_from_1_to_n.cpp

--- DSA Problems 1/counting_from_1_to_n.cpp
+++ DSA Problems 1/counting_from_1_to_n.cpp
@@ -18,3 +18,64 @@ int main()
     cout<<endl;
     return 0;
 }
+
+/* class Solution {
+public:
+int firstOcc( vector<int>& nums , int target ) 
+  {
+    int left = 0;
+    int right = nums.size() - 1;
+    int mid = left + (right - left) /2;
+    int ans = -1;
+
+     while ( left <= right ){
+        int mid = left + right / 2;
+
+        if ( target == nums[mid] ){
+             ans = mid ;
+             right = mid - 1;
+        }
+            else if ( target < nums[mid] ){
+                right = mid - 1;
+            }
+            else {
+            left = mid + 1;
+            }
+            mid = left + (right - left) /2;
+     }
+       return ans;
+}
+    int lastOcc ( vector<int>& nums , int target)
+{
+    int left = 0 ;
+    int right = nums.size() - 1;
+    int mid  = left + ( right-left )/2;
+    int ans = -1 ;
+
+    while ( left <= right ){
+
+        if ( target == nums[mid] ){
+            ans = mid;
+            left = mid + 1; 
+            }
+            else if ( target < nums[mid] ){
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+            mid = left + ( right-left )/2;
+    }
+    return ans;
+ } 
+    vector<int> searchRange(vector<int>& nums, int target) {
+      
+      vector<int>p;
+     int first= firstOcc( nums , target);
+     int second = lastOcc ( nums , target);
+
+     p.push_back(first);
+     p.push_back(second);
+      return p ;
+       }
+}; */
