31f032fe586f19bdbf2ee51c78347720300dfb95
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-03-11 22:06:05 +0530

    Update 32_all_duplicates_in_an_array.cpp

--- DSA Problems 1/32_all_duplicates_in_an_array.cpp
+++ DSA Problems 1/32_all_duplicates_in_an_array.cpp
@@ -10,11 +10,15 @@ void duplicates ( int arr[] , int n )
 
     for( int i = 0; i < n-1;i++)
     {
+       int temp = arr[i];
         if ( arr[i]==arr[i+1] )
         {
             cout << arr[i] <<" ";
-            i++;
+            while(i<n-1 && arr[i+1]==temp) {
+               i++;
+            }
         }
+       
     }
  }
  int main()
