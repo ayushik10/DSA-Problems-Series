39dfde8d3943403cec8edc6b8f8883d6b63e3dd3
p1kalys <pavanemani14@gmail.com>
2023-10-06 20:15:28 +0530

    Added a DP Problem

---
+++ DSA Problems Dynamic Programming/Integer_Break.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        std::vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        
+        for (int num = 2; num <= n; ++num) {
+            dp[num] = (num == n) ? 0 : num;
+            for (int i = 1; i < num; ++i) {
+                int val = dp[i] * dp[num - i];
+                dp[num] = std::max(dp[num], val);
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
+int main() {
+    int n;
+    std::cout << "Enter an integer n: ";
+    std::cin >> n;
+    
+    Solution solution;
+    int result = solution.integerBreak(n);
+    
+    std::cout << "Maximum product of integers for " << n << " is: " << result << std::endl;
+    
+    return 0;
+}
